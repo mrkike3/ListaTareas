8e2ae98fb11f755384cda8e2d5c60b12602dd534
mrkike <diegoperez_96@hotmail.com>
2022-10-08 16:15:38 -0300

    primer commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+
+import{saludar} from './js/componentes';
+import './styles.css';
+
+
+const nombre = 'joint';
+ saludar(nombre);
---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebPack = require('html-webpack-plugin');
+
+const CopyPlugin = require("copy-webpack-plugin");
+const MiniCssExtract = require("mini-css-extract-plugin");
+
+
+module.exports = {
+    mode:'development',
+
+     output:{
+        clean: true
+    },
+
+    module:{
+        rules:[
+            {
+                test: /\.html$/,
+                loader:'html-loader',
+                options: {
+                    sources: false
+                }
+            },
+            {
+                test:/\.css/,
+                exclude: /styles.css$/,
+                use:['style-loader','css-loader']
+            },
+            {
+                test: /styles.css$/,
+                use:[MiniCssExtract.loader, 'css-loader']
+            },
+            {
+                test:/\.(png|jpe?g|gif)$/,
+                loader: 'file-loader'
+            }
+        ]
+    },
+
+    optimization: {},
+
+
+    plugins:[
+        new HtmlWebpackPlugin({
+            title:'Mi Webpack App',
+            //filename:'index.html'
+            template:'./src/index.html'
+        }),
+
+        new MiniCssExtract({
+            filename: '[name].css',
+            ignoreOrder: false
+        }),
+
+        new CopyPlugin({
+            patterns: [
+                
+                {from: 'src/assets/', to: 'assets/'}
+            ]
+        })
+
+    ],
+}
